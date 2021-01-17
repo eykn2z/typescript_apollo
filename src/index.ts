c09941619b0a4a92e46e5f9bf7492e5225cd29ff
Akane Fujii <akane.f1991@gmail.com>
2021-01-17 14:26:22 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+import cors from 'cors';
+import { ApolloServer, gql, IResolvers } from 'apollo-server-express';
+
+const app = express();
+
+app.use(cors());
+
+interface User {
+  id: string;
+  username: string;
+}
+interface Users {
+  [key: string]: User;
+}
+
+const users: Users = {
+  '1': { id: '1', username: 'mkubara' },
+  '2': { id: '2', username: 'suzukalight' },
+};
+const me = users[1];
+
+const schema = gql`
+  type Query {
+    me: User
+    users: [User!]
+    user(id: ID!): User
+  }
+
+  type User {
+    id: ID!
+    username: String!
+  }
+`;
+
+const resolvers: IResolvers = {
+  Query: {
+    me: () => me,
+    users: () => Object.values(users),
+    user: (parent, { id }) => users[id] || null,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 4000 }, () => {
+  console.log('server on http://localhost:4000/graphql');
+});
